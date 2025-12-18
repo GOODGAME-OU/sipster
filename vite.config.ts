ab2bc9c86908922e132ab8ab224e16692f2ac744
Dmytro Soldatov <cyberdexx@gmail.com>
2025-12-18 18:48:42 +0200

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import appData from './package.json';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import ConditionalCompile from 'vite-plugin-conditional-compiler';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ command, mode }) => {
     console.log(
@@ -13,23 +14,53 @@ export default defineConfig(({ command, mode }) => {
 
     const isDev = mode === 'development';
 
+    const plugins = [
+        react(),
+        ConditionalCompile(),
+        createHtmlPlugin({
+            minify: !isDev,
+            entry: 'src/main.tsx',
+            template: 'index.html',
+            inject: {
+                data: {
+                    title: appData.settings.name,
+                    injectScript: `<script type="module" src="/src/main.tsx"></script>`,
+                },
+            },
+        }),
+    ];
+
+    if (!isDev) {
+        plugins.push(
+            VitePWA({
+                registerType: 'autoUpdate',
+                injectRegister: 'auto',
+                manifest: {
+                    name: 'Sipster',
+                    short_name: 'Sipster',
+                    start_url: '/sipster/',
+                    display: 'standalone',
+                    background_color: '#f3f7fc',
+                    theme_color: '#4da6ff',
+                    icons: [
+                        {
+                            src: '/assets/icon-192.png',
+                            sizes: '192x192',
+                            type: 'image/png'
+                        },
+                        {
+                            src: '/assets/icon-512.png',
+                            sizes: '512x512',
+                            type: 'image/png'
+                        }
+                    ]
+                }
+            }));
+    }
+
     return {
         base: isDev ? '/' : '/sipster/',
-        plugins: [
-            react(),
-            ConditionalCompile(),
-            createHtmlPlugin({
-                minify: !isDev,
-                entry: 'src/main.tsx',
-                template: 'index.html',
-                inject: {
-                    data: {
-                        title: appData.settings.name,
-                        injectScript: `<script type="module" src="/src/main.tsx"></script>`,
-                    },
-                },
-            }),
-        ],
+        plugins,
         define: {
             APP_ID: JSON.stringify(appData.name),
             APP_NAME: JSON.stringify(appData.settings.name),
