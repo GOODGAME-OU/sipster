54f613348be7d5986d68a7ac230efcbaf6112a6a
Dmytro Soldatov <cyberdexx@gmail.com>
2025-12-18 18:00:15 +0200

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import appData from './package.json';
-import manifest from './public/assets/manifest.json';
+// import manifest from './public/assets/manifest.json';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import ConditionalCompile from 'vite-plugin-conditional-compiler';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ command, mode }) => {
     console.log(
@@ -16,24 +16,24 @@ export default defineConfig(({ command, mode }) => {
     const isDev = mode === 'development';
 
     return {
-        base: '/sipster/',
+        base: isDev ? '/' : '/sipster/',
         plugins: [
             react(),
             ConditionalCompile(),
-            VitePWA({
-                includeAssets: [
-                    'assets/icons/favicon.ico',
-                    'assets/icons/apple-touch-icon.png',
-                    'assets/icons/favicon-96x96.png',
-                    'assets/icons/screenshot1.png',
-                ],
-                registerType: 'autoUpdate',
-                devOptions: {
-                    enabled: true,
-                },
-                injectRegister: 'auto',
-                manifest: manifest as any,
-            }),
+            // isDev ? undefined : VitePWA({
+            //     includeAssets: [
+            //         'assets/icons/favicon.ico',
+            //         'assets/icons/apple-touch-icon.png',
+            //         'assets/icons/favicon-96x96.png',
+            //         'assets/icons/screenshot1.png',
+            //     ],
+            //     registerType: 'autoUpdate',
+            //     devOptions: {
+            //         enabled: true,
+            //     },
+            //     injectRegister: 'auto',
+            //     manifest: manifest as any,
+            // }),
             createHtmlPlugin({
                 minify: !isDev,
                 entry: 'src/main.tsx',
